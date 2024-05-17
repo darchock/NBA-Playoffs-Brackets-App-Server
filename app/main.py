10d2f810acff843ee7f4b3aa710c9aed5b1a60bd
Itamar Aloni <itamaraloni@gmail.com>
2024-05-17 14:24:42 +0300

    flask and react integration

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def index():
-    return "Hello World :)"
-
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        'message': 'Hello from Broski!'
+    }
+    print("broski")
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
