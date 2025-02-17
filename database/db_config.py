43d09e86bf0bafb54e65e8781483386ed1b1eb06
darchock <darchock1@gmail.com>
2025-02-17 22:32:40 +0200

    added db_config for controling which db configuration we want to use.

---
+++ database/db_config.py
@@ -0,0 +1,12 @@
+from database.database_connector import DatabaseConnector
+
+# Database configuration
+DB_CONFIG = {
+    'host': 'database-nba-playoff-predictions-test-2025.cjsgac0qm87k.us-east-2.rds.amazonaws.com',
+    'database': 'db_test_2025',
+    'user': 'admin',
+    'password': 'rxrVkZzc7dwgi5JvMYer'
+}
+
+# Create a singleton instance of DatabaseConnector
+db = DatabaseConnector(**DB_CONFIG)
