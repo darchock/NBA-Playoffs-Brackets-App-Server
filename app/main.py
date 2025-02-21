06eb71caadd4aea8c1820d9621f086ecf160aec1
Itamar Aloni <itamaraloni@gmail.com>
2025-02-21 17:33:18 +0200

    new api to fetch team

--- app/main.py
+++ app/main.py
@@ -26,5 +26,18 @@ def get_users_data():
         logger.error(f"Error retrieving users: {str(e)}")
         return jsonify({"error": "Failed to retrieve users"}), 500
 
+@app.route('/api/teams/<string:team_name>', methods=['GET'])
+def get_team_data(team_name):
+    try:
+        print("here")
+        # Execute query using the database connector
+        team = db.execute_query("SELECT * FROM Team WHERE name LIKE '%Boston Celtics%';")
+        logger.info(f"Successfully retrieved {team} users from database")
+        print(f"team name: {team[0]['name']}")
+        return team
+    except Exception as e:
+        logger.error(f"Error retrieving users: {str(e)}")
+        return jsonify({"error": "Failed to retrieve users"}), 500
+
 if __name__ == "__main__":
     app.run(debug=True)
