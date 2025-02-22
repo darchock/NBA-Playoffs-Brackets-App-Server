e5a0d6f97abf657c3bc2a858c51c2bf8d6636b84
Itamar Aloni <itamaraloni@gmail.com>
2025-02-22 17:23:08 +0200

    models.py

---
+++ app/models/models.py
@@ -0,0 +1,17 @@
+from database.database_alchemy import get_db, engine
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import Session, declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    google_id = Column(String)
+
+# Then query using ORM:
+with Session(engine) as session:
+    users = session.query(User).all()
+    for user in users:
+        print(user.email)
