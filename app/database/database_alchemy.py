ec3f38558eb209de8ec2641d6eeff28cfa534d5a
Itamar Aloni <itamaraloni@gmail.com>
2025-02-28 17:51:19 +0200

    connect to local database instead of RDS

--- app/database/database_alchemy.py
+++ app/database/database_alchemy.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # MySQL URL format: "mysql+mysqlconnector://username:password@host:port/database_name"
-DATABASE_URL = "mysql+mysqlconnector://admin:rxrVkZzc7dwgi5JvMYer@database-nba-playoff-predictions-test-2025.cjsgac0qm87k.us-east-2.rds.amazonaws.com:3306/db_test_2025"
+# DATABASE_URL = "mysql+mysqlconnector://admin:rxrVkZzc7dwgi5JvMYer@database-nba-playoff-predictions-test-2025.cjsgac0qm87k.us-east-2.rds.amazonaws.com:3306/db_test_2025"
+DATABASE_URL = "mysql+mysqlconnector://root:DarchAlonz96@localhost:3306/db_test_2025"
 
 # Create the engine
 engine = create_engine(DATABASE_URL)
